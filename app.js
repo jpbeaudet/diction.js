e83bd6ddf62a227ad0075ab2854c8b33c9538664
U-user-PC\user <user@user-PC.(none)>
2014-09-27 18:36:29 -0400

    more

--- app.js
+++ app.js
@@ -65,14 +65,14 @@ app.listen(app.get('port'), function(){
   console.log(("Express server listening on port " + app.get('port')));
 });
 
-var io = require('socket.io').listen(4000);
-io.on('connection', function(socket){ 
-	//here will go the initial load of the current saved user data (last diction)
-	io.on("request",function(){
+//var io = require('socket.io').listen(server);
+//io.on('connection', function(socket){ 
+	////here will go the initial load of the current saved user data (last diction)
+	//io.on("request",function(){
 		
-	});
+	//});
 	
-});
+//});
 
-server.listen(4000);
+
 server.listen(3000);
