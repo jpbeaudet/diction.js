4f01f6d5aa2999686e6c3116b9b5ec1da96a5554
U-user-PC\user <user@user-PC.(none)>
2014-10-16 16:43:34 -0400

    another strategy

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var options = {
 		options.passphrase = passphrase;
 	}
 
-
+var cookieParser = express.cookieParser('J976dd78Hffr#$%68h');
 // main config
 var app = express();
 //var server = http.createServer(app);
@@ -40,7 +40,7 @@ app.set('view options', { layout: false });
 app.use(express.logger());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('J976dd78Hffr#$%68h'));
+app.use(cookieParser);
 app.use(express.session({
 	  secret: '%%?7hhh%43SS_--$',
 	  store: new MongoStore({
@@ -85,6 +85,38 @@ console.log(("Express server listening on port " + app.get('port')));
 
 
 var io = require('socket.io').listen(server);
+io.set('authorization', function (data, callback) {
+    if(!data.headers.cookie) {
+        return callback('No cookie transmitted.', false);
+    }
+
+    // We use the Express cookieParser created before to parse the cookie
+    // Express cookieParser(req, res, next) is used initialy to parse data in "req.headers.cookie".
+    // Here our cookies are stored in "data.headers.cookie", so we just pass "data" to the first argument of function
+    cookieParser(data, {}, function(parseErr) {
+        if(parseErr) { return callback('Error parsing cookies.', false); }
+
+        // Get the SID cookie
+        var sidCookie = (data.secureCookies && data.secureCookies[EXPRESS_SID_KEY]) ||
+                        (data.signedCookies && data.signedCookies[EXPRESS_SID_KEY]) ||
+                        (data.cookies && data.cookies[EXPRESS_SID_KEY]);
+
+        // Then we just need to load the session from the Express Session Store
+        sessionStore.load(sidCookie, function(err, session) {
+            // And last, we check if the used has a valid session and if he is logged in
+            if (err || !session || session.isLogged !== true) {
+                callback('Not logged in.', false);
+            } else {
+                // If you want, you can attach the session to the handshake data, so you can use it again later
+                // You can access it later with "socket.handshake.session"
+                data.session = session;
+
+                callback(null, true);
+            }
+        });
+    });
+});
+
 io.on('connection', function(socket){ 
 	var memory ="";
 	console.log("socket.io started on port"+ app.get('port'));
@@ -97,11 +129,7 @@ io.on('connection', function(socket){
 			console.log("socket save = "+ data);
 		   memory += data + " ";
 			console.log("memory = "+ memory);	
-			req.session.doc= memory
-			exports.save = function (req, res) {
-			 req.session.doc += memory;
-			 console.log("req.session.doc = "+ req.session.doc);
-				};
+
 		});
 	
 	////here will go the initial load of the current saved user data (last diction)
