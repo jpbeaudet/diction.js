8937d1fdd3b1d33c71e6ea05e898349547bf560d
U-user-PC\user <user@user-PC.(none)>
2014-10-16 14:52:23 -0400

    session46

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var http = require('http')
-  , path = require('path')
-  , connect = require('connect')
-  , express = require('express')
-  , app = express();
+// Author: Jean-Philippe Beaudet @ S3R3NITY Technology
+// Diction4js - Web based hands-free text editor
+//
 
+
+// dependencies
+
+var path = require('path');  
+var express = require('express');
+var http = require('http');
 var https = require('https');
-var session  = require('express-session');
-//var MongoStore = require('connect-mongo')(express);
+var session    = require('express-session');
+var MongoStore = require('connect-mongo')(express);
 var mongoose = require('mongoose');
 var passport = require('passport');
 var fs = require('fs');
 var LocalStrategy = require('passport-local').Strategy;
-//var MemoryStore = express.session.MemoryStore;
-//var sessionStore = new MemoryStore();
 var passphrase = "";
 
 var options = {
@@ -25,48 +27,89 @@ var options = {
 		options.passphrase = passphrase;
 	}
 
-var cookieParser = express.cookieParser('your secret sauce')
-  , sessionStore = new connect.middleware.session.MemoryStore();
 
+// main config
+var app = express();
+//var server = http.createServer(app);
+//var server = require('http').Server(app);
+//app.set('port', process.env.PORT || 3000);
 app.configure(function () {
-  app.set('views', path.resolve('views'));
-  app.set('view engine', 'jade');
-  app.set('view options', { layout: false });
-  app.use(express.logger());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(cookieParser);
-  app.use(express.session({ store: sessionStore }));
-  app.use(app.router);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('J976dd78Hffr#$%68h'));
+app.use(express.session({
+	  secret: '%%?7hhh%43SS_--$',
+	  store: new MongoStore({
+		    host: '127.0.0.1',
+		    port: 27017,
+		    db: 'diction4js'
+		    
+		  })
+		}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/app", express.static(__dirname + "/app"));
 });
 
-//routes
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport_local_mongoose');
+
+// routes
 require('./routes')(app);
 
-var server = http.createServer(options,app)
-  , io = require('socket.io').listen(server);
+var server = https.createServer(options, app);
 app.set('port', process.env.PORT || 3000);
-
-var SessionSockets = require('session.socket.io')
-  , sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
+console.log(("Express server listening on port " + app.get('port')));
 
 
-sessionSockets.on('connection', function (err, socket, session) {
+
+
+var io = require('socket.io').listen(server);
+io.on('connection', function(socket){ 
+	var memory;
 	console.log("socket.io started on port"+ app.get('port'));
     
-	socket.on("request",function(data){
-		console.log("socket answer = "+ data);
-		socket.emit("response", [ "docA" ,"docB"]);
-	});
-	socket.on("save",function(data){
-		console.log("socket save = "+ data);
+		socket.on("request",function(data){
+			console.log("socket answer = "+ data);
+			socket.emit("response", [ "docA" ,"docB"]);
+		});
+		socket.on("save",function(data){
+			console.log("socket save = "+ data);
+		   memory = data;
+			console.log("session = "+ memory);
+			 
+		});
+	
+	////here will go the initial load of the current saved user data (last diction)
+
 		
-		session.docA += data;
-		//session.doc += data; 
-		session.save();
-		console.log("session = "+ session.docA);
-		 
-	});
+	app.get('/save', function(req, res) {
+		  req.session.doc += memory ;
+		  console.log("session = "+ req.session.doc);
+		  res.end();
+	  });
+	
 });
+
 
 server.listen(3000);
