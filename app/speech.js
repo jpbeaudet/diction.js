3b814a45b3666edf8b6899aba53b234dbdea4d66
U-user-PC\user <user@user-PC.(none)>
2014-10-19 14:25:05 -0400

    comiting toggl cganh

--- app/speech.js
+++ app/speech.js
@@ -29,7 +29,7 @@ if ('webkitSpeechRecognition' in window) {
       for (var i = 0; i < event.results.length; ++i) {
         if (event.results[i].isFinal) {
         	$("#final_span").css("color", "grey");
-        	toggleStartStop() ;
+        	
             txt = event.results[i][0].transcript;
         	interim = ""; 
         	var isCmd="";
