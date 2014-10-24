8a09cbf2b87272ae6ebe65c30196b9d91bebecf5
U-user-PC\user <user@user-PC.(none)>
2014-10-24 19:20:32 -0400

    revert for now , the text area solution is too ugly

--- app/speech.js
+++ app/speech.js
@@ -41,7 +41,7 @@ if ('webkitSpeechRecognition' in window) {
       final_span.innerHTML = " @@@->  " + txt + "  <-@@@ ";
       interim_span.innerHTML = interim;
       icon_span.innerHTML = "-->";
-      manual_span.innerHTML = '<textarea name ="manual" rows="1" cols="1"></textarea>';
+      //manual_span.innerHTML = '<textarea name ="manual" rows="1" cols="1"></textarea>';
       
     }; 
 
