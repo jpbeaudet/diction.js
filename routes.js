85809a676b2a1913892c9bb4cf8530b9c6395d19
U-user-PC\user <user@user-PC.(none)>
2014-10-24 17:46:08 -0400

    more tiny

--- routes.js
+++ routes.js
@@ -59,6 +59,11 @@ app.get('/login', function(req, res) {
 	  res.sendfile('./node_modules/tinymce/tinymce.min.js');
 	});
   
+  app.get('/node_modules/tinymce/themes/modern/theme.min.js', function(req, res) {
+	  res.set('Content-Type', 'text/javascript');
+	  res.sendfile('./node_modules/tinymce/themes/modern/theme.min.js');
+	});
+  
   app.get('/data.js', function(req, res) {
 	  res.set('Content-Type', 'text/javascript');
 	  res.sendfile('./data.js');
