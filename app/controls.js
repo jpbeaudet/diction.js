e4f91c1a4280c3eb86bc7defe2148c9e0c639997
U-user-PC\user <user@user-PC.(none)>
2014-11-11 22:28:01 -0500

    getting square bracket back

--- app/controls.js
+++ app/controls.js
@@ -392,20 +392,21 @@ window.onload = function()
 				  break;
 				  default:
 					  return Isfalse(transcript, index); 
-			 }	
-		 //case 'square':
-			 //switch(Tword)
-			// {
+			 }
+			//[
+		 case 'square':
+			 switch(Tword)
+			 {
 			 
-			// case 'bracket':
-				 // $("#final_span").css("color", "pink");
-				 // docA_span.innerHTML = index.docA+ '[';
-				 // index.docA += '[';
-				 // return Istrue("Open Square Bracket", index);
-				 // break;
-				 // default:
-					//  return Isfalse(transcript, index); 
-			// }			 
+			 case 'bracket':
+				  $("#final_span").css("color", "pink");
+				  docA_span.innerHTML = index.docA+ '[';
+				  index.docA += '[';
+				  return Istrue("Open Square Bracket", index);
+				  break;
+				  default:
+					  return Isfalse(transcript, index); 
+			 }			 
 		  default:
 			  return Isfalse(transcript, index);   
 	 }		 
@@ -457,25 +458,26 @@ window.onload = function()
 				  default:
 					  return Isfalse(transcript, index); 
 			 }
-		// case 'square':
-		//	 switch(Tword)
-		//	 {
+			 //]
+		 case 'square':
+			 switch(Tword)
+			 {
 			 
-		//	 case 'bracket':
-		//		  $("#final_span").css("color", "pink");
-		//		  docA_span.innerHTML = index.docA+ ']';
-		//		  index.docA += ']';
-		//		  return Istrue("Close Square Bracket", index);
-		//		  break;
-		//		  default:
-		//			  return Isfalse(transcript, index); 
-			// }				 
+			 case 'bracket':
+				  $("#final_span").css("color", "pink");
+				  docA_span.innerHTML = index.docA+ ']';
+				  index.docA += ']';
+				  return Istrue("Close Square Bracket", index);
+				  break;
+				  default:
+					  return Isfalse(transcript, index); 
+			 }				 
 		  default:
 			  return Isfalse(transcript, index);   
 	 }		 
 
-	//[
-    //]
+	
+   
 	// +
 	// -
 	// =
