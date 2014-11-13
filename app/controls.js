3cd0b4ef1699ef3fb381fbcbe99f56ba5ca438ac
U-user-PC\user <user@user-PC.(none)>
2014-11-12 20:55:19 -0500

    imore for mmove back almost there

--- app/controls.js
+++ app/controls.js
@@ -749,7 +749,7 @@ window.onload = function()
 				  if(word[n-1]== ""){word.splice(n-1,1);}
 				  if(word[n-1]== ""){word.splice(n-1,1);}
 				  if(word[n-1]== ""){word.splice(n-1,1);}
-				  index.docB = index.docB + word[n-1]+ " ";
+				  index.docB = word[n-1]+" "+ index.docB   ;
 				  console.log("word[n-1] = "+ word[n-1]);
 				  index.docA = index.docA.replace(/  /g," ");
 				  index.docA = index.docA.replace(word[n-1],"");
