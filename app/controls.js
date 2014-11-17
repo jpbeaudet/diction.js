7e869f8cf06f596c0be7b1f514a1e269a0e8cc26
U-user-PC\user <user@user-PC.(none)>
2014-11-17 17:56:04 -0500

    semi revert

--- app/controls.js
+++ app/controls.js
@@ -847,7 +847,7 @@ window.onload = function()
 				  //var toDelete = word[n-1].split(" ");
 				  var toDelete = word[n].split(" ");
 				  for( x in toDelete ){
-					  index.docA = index.docA.replace('/'+toDelete[x]+'/g',""); 
+					  index.docA = index.docA.replace(toDelete[x],""); 
 					  index.docA = index.docA.replace(/  /g," ");
 				  }
 				  
