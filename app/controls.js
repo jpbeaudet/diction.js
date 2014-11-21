47fab1d7d4e255ce94cb706d78a95e6db5b1df55
U-user-PC\user <user@user-PC.(none)>
2014-11-21 14:12:17 -0500

    testing my sleepy solution

--- app/controls.js
+++ app/controls.js
@@ -1189,7 +1189,7 @@ window.onload = function()
 		  docB_span.innerHTML = index.LastdocB;
 		  index.docA = index.LastdocA;
 		  index.docB = index.LastdocB;
-		  return Istrue('Cancel', index);
+		 // return Istrue('Cancel', index);
 		  break;
 		  default:
 			  return Isfalse(transcript, index);   
@@ -1206,7 +1206,7 @@ window.onload = function()
 		  docB_span.innerHTML = index.LastdocB;
 		  index.docA = index.LastdocA;
 		  index.docB = index.LastdocB;
-		  return Istrue('Cancel', index);
+		  //return Istrue('Cancel', index);
 		  break;
 		  default:
 			  return Isfalse(transcript, index);   
@@ -1223,7 +1223,7 @@ window.onload = function()
 		  docB_span.innerHTML = index.LastdocB;
 		  index.docA = index.LastdocA;
 		  index.docB = index.LastdocB;
-		  return Istrue('Cancel', index);
+		 // return Istrue('Cancel', index);
 		  break;
 		  default:
 			  return Isfalse(transcript, index);   
