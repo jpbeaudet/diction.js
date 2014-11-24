2194d3421fd54240ac4d404d57b7d29597a9361f
U-user-PC\user <user@user-PC.(none)>
2014-11-24 18:50:28 -0500

    forgot to add lib.js

---
+++ app/lib.js
@@ -0,0 +1,6 @@
+function _tab(transcript,index){
+	  $("#final_span").css("color", "pink");
+	  docA_span.innerHTML = index.docA+ '&nbsp;&nbsp;&nbsp;&nbsp;';
+	  index.docA =  index.docA+ '&nbsp;&nbsp;&nbsp;&nbsp;';
+	  return Istrue("Tab", index);
+}
