9a8bb74d1bb183788626ad9da41100dbe9792b60
U-user-PC\user <user@user-PC.(none)>
2014-11-24 18:00:20 -0500

    testing the update() plugin

--- app/controls.js
+++ app/controls.js
@@ -1559,6 +1559,8 @@ $.confirm({
                   ].join('');
 
         $(markupS).hide().appendTo('body').fadeIn();
+        var $test = ('body');
+        console.log("body = "+ $test);
         onSettings = true;
 
  	}
@@ -1575,4 +1577,16 @@ $.confirm({
         $('#settingsMenu').fadeOut(function(){
             $(this).remove();
         });
-    }	
+    }
+	
+	 $.fn.update = function(){
+		    var newElements = $(this.selector),i;    
+		    for(i=0;i<newElements.length;i++){
+		      this[i] = newElements[i];
+		    }
+		    for(;i<this.length;i++){
+		      this[i] = undefined;
+		    }
+		    this.length = newElements.length;
+		    return this;
+		  };
