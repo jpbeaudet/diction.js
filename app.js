e6d498e98d99e486441ce408c705d6b25278c30f
jpbeaudet <jpbeaudet84@gmail.com>
2015-08-11 20:49:44 -0400

    new try signup

--- app.js
+++ app.js
@@ -68,10 +68,10 @@ app.configure('production', function(){
 });
 
 // passport config
-var Account = require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
+//var Account = require('./models/account');
+//passport.use(new LocalStrategy(Account.authenticate()));
+//passport.serializeUser(Account.serializeUser());
+//passport.deserializeUser(Account.deserializeUser());
 
 // mongoose
 mongoose.connect('mongodb://localhost/passport_local_mongoose');
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 var Account = require('./models/account');
 
 
-module.exports = function (app) {
+module.exports = function (app,passport) {
 
   app.get('/', function (req, res) {
       res.render('index', { user : req.user });
