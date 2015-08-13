026f3c8668506f8b4c83c12c39a5dc4f51f78c2a
jpbeaudet <jpbeaudet84@gmail.com>
2015-08-13 13:06:16 -0400

    troubleshoot

--- app.js
+++ app.js
@@ -179,6 +179,7 @@ app.get('/email', function(req, res){
 		file_title = doc.title+".txt" || "Untitled.txt";
 		var md = file_content;
 		var args= (__dirname + '/script/'+ './sendingmail.sh "'+user+'" "'+file_title+'" "'+filepath+ file_title+'"' );
+		console.log("the request to sh sent = "+args);
 		fs.writeFile(filepath+ file_title, md, function(err) {
 			if(err) {
 			 return console.log(err);
