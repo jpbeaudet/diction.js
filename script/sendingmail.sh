063f8c5a53b7f8ec13f67cbdc769b172122dcde3
Jean-Philippe Beaudet <jpbeaudet@ip-172-31-47-39.us-west-2.compute.internal>
2015-12-06 23:07:04 +0000

    addin bootstrapwq

--- script/sendingmail.sh
+++ script/sendingmail.sh
@@ -3,8 +3,14 @@ EMAIL=$1
 SUBJECT=$2
 CONTENT=$3
 
-
+if [ "$#" -ne 3 ]; then 
+    echo "illegal number of parameters"
+fi
 
 echo "$EMAIL $SUBJECT $CONTENT"
 mail -s "$SUBJECT" "$EMAIL" < $CONTENT
 echo "Text content sent to $EMAIL"
+if [ -f $CONTENT ]; then
+rm $CONTENT
+echo " File $CONTENT DELETED"
+fi
