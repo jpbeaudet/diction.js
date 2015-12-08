2d0f9605ce7f156f3d2246c56d886dd3ebe84fac
jpbeaudet <jpbeaudet84@gmail.com>
2015-12-07 20:24:10 -0500

    testoing oups with route and message

--- routes.js
+++ routes.js
@@ -23,7 +23,9 @@ module.exports = function (app) {
   app.get('/register', function(req, res) {
       res.render('register', { });
   });
-
+  app.get('/oups', function(req, res) {
+      res.render('oups', {message: "You need to use Google Chrome or Chromium to use smartnotes."});
+  });
   
    app.post('/register', function(req, res,next) {
        req.assert('username', 'required').notEmpty();
